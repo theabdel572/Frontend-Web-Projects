bc2c9b6a97de27902df223de4656e027d2d44266
theabdel572 <abdelkarimachbaniabid724@gmail.com>
2023-08-27 03:21:27 +0200

    Added a whole new project (Dashboard Layout).

---
+++ Dashboard Layout/README.md
@@ -0,0 +1,10 @@
+# Dashboard layout
+### A responsive dashboard layout webpage.
+
+## Technologies used
+This layout is made using HTML, CSS, and JavaScript.
+
+## Skills
+I've made this project to practice my CSS skills, focusing on
+responsive design and UI design concepts, but also to reinforce
+some HTML and JavaScript.
---
+++ Dashboard Layout/index.js
@@ -0,0 +1,28 @@
+// javascript
+
+const get = element => document.getElementById(element);
+
+let open = get("menu-btn");
+let nav = get("nav");
+let exit = get("exit-btn");
+
+open.addEventListener('click', () => {
+    nav.classList.add('open-nav');
+})
+
+exit.addEventListener('click', () => {
+    nav.classList.remove('open-nav');
+})
+
+
+// show viewport width
+
+let width = document.getElementById('width');
+const onresize = function () {
+    width.innerText = `${document.body.clientWidth}`;
+    width.classList.add('display-width');
+    setTimeout(() => {
+        width.classList.remove('display-width');
+    }, 2000)
+};
+window.addEventListener("resize", onresize);
