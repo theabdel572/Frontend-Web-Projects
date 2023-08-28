c8b9d707ae4c81ca8d3d562fcbb380b897618fcf
theabdel572 <abdelkarimachbaniabid724@gmail.com>
2023-08-29 00:34:25 +0200

    Added a whole new project (AirBnB Experiences Clone).

---
+++ AirBnB Experiences Clone/README.md
@@ -0,0 +1,8 @@
+# AirBnB Experiences Clone
+### A similar layout to the AirBnB Experiences page.
+
+## Technologies Used
+The project is made using HTML, CSS, JavaScript, JSX, React and Vite.
+
+## Skills
+I've made this project to reinforce my knowledge of React and JSX.
---
+++ AirBnB Experiences Clone/src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react"
+import Navbar from "./components/Navbar.jsx"
+import Hero from "./components/Hero.jsx"
+import Card from "./components/Card.jsx"
+import data from "./data"
+
+export default function App() {
+    const cards = data.map(item => {
+        return (
+            <Card
+                key={item.id}
+                {...item}
+            />
+        )
+    })        
+    
+    return (
+        <div>
+            <Navbar />
+            <Hero />
+            <section className="cards-list">
+                {cards}
+            </section>
+        </div>
+    )
+}
